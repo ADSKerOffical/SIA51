7a0dacbf1a9e8c48ac47060178c2b7f5cb22f6e8
ADSKerOffical <137511182+ADSKerOffical@users.noreply.github.com>
2025-02-03 14:26:03 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -10,6 +10,7 @@ local Tab = Window:MakeTab({
 
 game.Players.LocalPlayer.leaderstats.Cell:GetPropertyChangedSignal("Value"):Connect(function()
  if game.Players.LocalPlayer.leaderstats.Cell.Value <= -1 then
+game.Players.LocalPlayer.leaderstats.Cell.Value = 1
    game:Shutdown()
  end
 end)
@@ -121,9 +122,31 @@ end
    end    
 })
 
+Tab:AddDropdown({
+	Name = "Teleport",
+	Default = "1",
+	Options = {"Spawn", "Portal", "Incinerator", "Metro", "Sewerage", "Reaper door"},
+	Callback = function(Value)
+		if Value == "Spawn" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(709, 1150, 744)
+		elseif Value == "Portal" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-101, 982, 805)
+		elseif Value == "Incinerator" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-322, 1062, 1159)
+		elseif Value == "Metro" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-469, 922, 749)
+		elseif Value == "Sewerage" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-495, 916, -56)
+		elseif Value == "Reaper door" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-202, 982, 294)
+		end
+	end    
+})
+
 Tab:AddButton({
- Name = "Unpack all boxes",
+ Name = "Unpack all supply box",
  Callback = function()
+local saved = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 for _, promp in ipairs(workspace:GetDescendants()) do
   if promp:IsA("ProximityPrompt") and promp.Parent.Name == "SupplyBox" then
 promp.MaxActivationDistance = 100
@@ -135,6 +158,72 @@ game.Workspace[game.Players.LocalPlayer.Name]:SetPrimaryPartCFrame(promp.Parent.
   task.wait(0.2)
 fireproximityprompt(promp, 1e9)
  end
+end
+ task.wait(0.1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = saved
+   end    
+})
+
+Tab:AddToggle({
+ Name = "Spam tools",
+ Default = false,
+ Callback = function(Value)
+ Sorting = Value
+while Sorting do
+  for _, item in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+    if item:IsA("Tool") then
+      game.Players.LocalPlayer.Character.Humanoid:EquipTool(item)
+      game:GetService("RunService").RenderStepped:wait()
+      game.Players.LocalPlayer.Character.Humanoid:UnequipTools()
+    end
+  end
+   task.wait()
+end
+   end    
+})
+
+Tab:AddButton({
+ Name = "Hit all entities",
+ Callback = function()
+for _, mod in ipairs(workspace:GetChildren()) do
+  if mod:IsA("Model") and mod:FindFirstChildOfClass("Humanoid") then
+ local humanoid = mod:FindFirstChildOfClass("Humanoid")
+local args = {
+    [1] = "Head",
+    [2] = humanoid,
+    [3] = humanoid.Parent.Head,
+    [4] = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+    [5] = Vector3.new(0.937201201915741, -0.3045220375061035, -0.17005980014801025)
+}
+
+game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").GunScript_Server.InflictTarget:FireServer(unpack(args))
+  end
+end
+   end    
+})
+
+Tab:AddToggle({
+ Name = "Hit all entities (loop)",
+ Default = false,
+ Callback = function(Value)
+ oop = Value
+while oop and task.wait(0.1) do
+ pcall(function()
+ for _, mod in ipairs(workspace:GetChildren()) do
+  if mod:IsA("Model") and mod:FindFirstChildOfClass("Humanoid") then
+ local humanoid = mod:FindFirstChildOfClass("Humanoid")
+local args = {
+    [1] = "Head",
+    [2] = humanoid,
+    [3] = humanoid.Parent.Head,
+    [4] = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+    [5] = Vector3.new(0.937201201915741, -0.3045220375061035, -0.17005980014801025)
+}
+
+game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool").GunScript_Server.InflictTarget:FireServer(unpack(args))
+  end
+ end
+ end)
 end
    end    
 })
